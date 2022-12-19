acff5257d78a29b08fbb597d5516407b7d795634
{SS66ah} <{satsuki0606yj@yahoo.co.jp}>
2022-12-19 14:13:46 +0900

    Fix:修正
    登録された順に表示されるように修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,12 @@ class BooksController < ApplicationController
     def index
         
         if params[:search] == nil
-            @books = Book.all.page(params[:page]).per(20)
+            @books = Book.all.page(params[:page]).per(20).order("created_at DESC")
         elsif params[:search] == ''
-            @books = Book.all.page(params[:page]).per(20)
+            @books = Book.all.page(params[:page]).per(20).order("created_at DESC")
         else
             #部分検索
-            @books = Book.where("title LIKE ? ",'%' + params[:search] + '%').page(params[:page]).per(20)
+            @books = Book.where("title LIKE ? ",'%' + params[:search] + '%').page(params[:page]).per(20).order("created_at DESC")
         end
     end
 
