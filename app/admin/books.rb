be69bd5043270d8d7cc90febc7a8799c544dc1ed
{SS66ah} <{satsuki0606yj@yahoo.co.jp}>
2023-01-10 12:01:24 +0900

    Add:管理者画面に詳細画面を追加

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -16,5 +16,36 @@ ActiveAdmin.register Book do
   # end
 
   permit_params :title, :author, :publisher, :year, :isbn, :image,:user_id, :rental_user_id
-  
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :isbn
+    column :year
+    column :author
+    column :publisher
+    actions
+  end
+  show do
+    attributes_table do
+      row 'タイトル' do
+        resource.title
+      end
+      row 'ISBN' do
+        resource.isbn
+      end
+      row '出版日' do
+        resource.year
+      end
+      row '著者' do
+        resource.author
+      end
+      row '出版社' do
+        resource.publisher
+      end
+      row '目次' do
+        resource.content
+      end
+    end
+  end
 end
